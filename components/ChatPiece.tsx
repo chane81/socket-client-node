a855dabc5d9828c28dc17a21580b4ba99065c455
chane81 <chane81@naver.com>
2019-03-25 07:08:31 +0900

    충돌 해결

--- components/ChatPiece.tsx
+++ components/ChatPiece.tsx
@@ -20,12 +20,7 @@ const ChatPiece: React.FC<IProps> = (props: IProps) => {
 				: 'chat-left'}`}
 		>
 			<div className={'chat-msg'}>
-				<UserPicture
-					isShadow={false}
-					nickId={props.nickId}
-					margin={'0.1rem 0.3rem 0.1rem -0.5rem'}
-					isRead={true}
-				/>
+				<UserPicture isShadow={false} isRead={true} />
 				<div>
 					<span>
 						<div dangerouslySetInnerHTML={{ __html: msg }} />
